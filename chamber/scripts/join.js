a8e735e27433aeecac4878c26025ebc56784ded3
atherbeauta <kmuamba2@byupathway.edu>
2026-02-04 16:12:34 +0200

    join

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,17 +1,11 @@
-// Timestamp
-document.getElementById("timestamp").value =
-  new Date().toISOString();
+document.addEventListener("DOMContentLoaded", () => {
+  // Timestamp
+  document.getElementById("timestamp").value = new Date().toISOString();
 
-// Modals
-document.querySelectorAll("[data-modal]").forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document.getElementById(link.dataset.modal).showModal();
-  });
-});
-
-document.querySelectorAll("dialog button").forEach(btn => {
-  btn.addEventListener("click", () => {
-    btn.closest("dialog").close();
+  // Modals
+  document.querySelectorAll("[data-modal]").forEach(button => {
+    button.addEventListener("click", () => {
+      document.getElementById(button.dataset.modal).showModal();
+    });
   });
 });
